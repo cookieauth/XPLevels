6ad112b6bafb6f4ee8346ada35b5fd2c07e17c7e
solsk <hi@solsk.sh>
2023-04-30 15:23:38 -0700

    AdventureAPI Switch
    Switch Titles and Chat messages to AdventureAPI. Natively found in Paper 1.16.5 and later.

--- src/main/java/sh/solsk/xplevels/XPLevelEvent.java
+++ src/main/java/sh/solsk/xplevels/XPLevelEvent.java
@@ -1,5 +1,10 @@
 package sh.solsk.xplevels;
 
+import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.title.Title;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Effect;
@@ -10,6 +15,9 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLevelChangeEvent;
+import org.jetbrains.annotations.NotNull;
+
+import java.time.Duration;
 
 public class XPLevelEvent implements Listener {
 
@@ -21,18 +29,10 @@ public class XPLevelEvent implements Listener {
 
         if (!(event.getNewLevel() < event.getOldLevel())) {
             // Send the player a message if enabled.
-            if (configuration.getBoolean("messages.chat.enabled"))
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&', replace("chat", player, event)));
+            if (configuration.getBoolean("messages.chat.enabled")) sendMessage(player, player, event);
 
             // Send the player a title & subtitle if enabled.
-            if (configuration.getBoolean("messages.titles.enabled")) {
-                player.resetTitle();
-                player.sendTitle(ChatColor.translateAlternateColorCodes('&', replace("title", player, event)),
-                        ChatColor.translateAlternateColorCodes('&', replace("subtitle", player, event)),
-                        configuration.getInt("messages.titles.fade-in"),
-                        configuration.getInt("messages.titles.stay"),
-                        configuration.getInt("messages.titles.fade-out"));
-            }
+            if (configuration.getBoolean("messages.titles.enabled")) showTitle(player, player, event);
 
             // Send a sound effect to the player if enabled.
             if (configuration.getBoolean("effects.sounds.enabled"))
@@ -75,5 +75,24 @@ public class XPLevelEvent implements Listener {
                     .replace("{xp-to-level}", String.valueOf(player.getExpToLevel()));
         }
         return "Ruh-roh! There was an error, if you are an administrator, please contact the developer immediately!";
+    }
+
+
+
+    private void showTitle(final @NotNull Audience audience, Player player, PlayerLevelChangeEvent event) {
+        audience.clearTitle();
+
+        final Component mainTitle = MiniMessage.miniMessage().deserialize(replace("title", player, event)).decoration(TextDecoration.ITALIC, false);
+        final Component subTitle = MiniMessage.miniMessage().deserialize(replace("subtitle", player, event)).decoration(TextDecoration.ITALIC, false);
+
+        final @NotNull Title.Times times = Title.Times.times(Duration.ofMillis(configuration.getLong("messages.titles.fade-in")), Duration.ofMillis(configuration.getLong("messages.titles.stay")), Duration.ofMillis(configuration.getLong("messages.titles.fade-out")));
+
+        Title title = Title.title(mainTitle, subTitle, times);
+
+        audience.showTitle(title);
+    }
+
+    private void sendMessage(final @NotNull Audience audience, Player player, PlayerLevelChangeEvent event) {
+        audience.sendMessage(MiniMessage.miniMessage().deserialize(replace("chat", player, event)).decoration(TextDecoration.ITALIC, false));
     }
 }
